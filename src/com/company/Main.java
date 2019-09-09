f2ffe18d09b6b185063e8a5b290ee342b4d6675c
diaye <yerim-oumar.dia@ssfs.org>
2019-09-09 14:08:01 -0400

    Practiced java characters

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,5 +6,8 @@ public class Main {
 	// write your code here
         System.out.println("Hello, world");
         System.out.println("My name is Bob");
+
+        System.out.println("This is \"Brian's\" computer");
+        System.out.println("I am splitting this \n over two lines");
     }
 }
