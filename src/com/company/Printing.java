a9b66dc6e9d128a5c238482ec5dc52c220c24413
diaye <yerim-oumar.dia@ssfs.org>
2019-09-10 14:46:31 -0400

    Finished Part 1 of 1.1 exercises

--- src/com/company/Printing.java
+++ src/com/company/Printing.java
@@ -3,5 +3,15 @@ package com.company;
 public class Printing {
     public static void main(String[] args) {
 
+        System.out.println("The time is 02:37");
+        System.out.println("The date is 09/10/2019");
+
+        System.out.println("It is a great \ntime to be a \ncomputer programmer!");
+
+        System.out.print("It is a great ");
+        System.out.print("time to be a ");
+        System.out.print("computer programmer");
+
+
     }
 }
