29287269add433fadf4a36d67c6528b693df5e1c
diaye <yerim-oumar.dia@ssfs.org>
2019-09-10 15:10:03 -0400

    Last step of Part 2

---
+++ src/com/company/Escape.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Escape {
+    public static void main(String[] args) {
+        System.out.println("What is the difference between");
+        System.out.println("a ' and a \"?  Or between a \" and a \\\"?");
+        System.out.println("One is what we see when we're typing our program.");
+        System.out.println(" The other is what appears on the \"console\" ");
+
+        System.out.print("  \\");
+        System.out.println("/");
+        System.out.print(" \\");
+        System.out.print("\\");
+        System.out.println("//");
+        System.out.print("\\");
+        System.out.print("\\");
+        System.out.print("\\");
+        System.out.print("\\");
+
+    }
+}
