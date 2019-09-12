dfa87b33eaa7bf9374d5e47ece3df63a4fb8c28f
diaye <yerim-oumar.dia@ssfs.org>
2019-09-12 08:53:19 -0400

    Finished Meta class file in Part 2

---
+++ src/com/company/Meta.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Meta {
+    public static void main(String[] args) {
+        System.out.println("public class Hello {");
+        System.out.println("  public static void main(String[] args) {");
+        System.out.println("        System.out.println(\"Hello, world!\")");
+        System.out.println("    {");
+        System.out.println("{");
+    }
+}
