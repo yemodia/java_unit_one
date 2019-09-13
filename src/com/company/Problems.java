52dabcee03a2192e5430d303625f52c096d8ed63
diaye <yerim-oumar.dia@ssfs.org>
2019-09-13 09:36:07 -0400

    Finished solving the problems in the code.

---
+++ src/com/company/Problems.java
@@ -0,0 +1,21 @@
+// ********************************************
+//   Problems.java
+//
+//   There are several error in the code below. Fix them all to get things to run
+//   Hopefully you can see that IDEA can be a huge help with this.
+/*
+ ********************************************
+*/
+package com.company;
+public class Problems
+{
+    public static void main(String[] args)
+    {
+        System.out.println ("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        System.out.println ("This program used to have lots of problems,");
+                System.out.println ("but if it prints this, you fixed them all.");
+        System.out.println ("      *** Hurray! ***");
+                System.out.println ("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+    }
+}
+
