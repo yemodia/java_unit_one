231bdff0bf3a9e79f929443131ee5cc5d50a1084
diaye <yerim-oumar.dia@ssfs.org>
2019-09-16 10:50:32 -0400

    Finished calculating the area of the rectangle

---
+++ src/com/company/RectangleArea.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class RectangleArea {
+    public static void main(String[] args) {
+        int length = 14;
+        int width = 6;
+        int area = width * length ;
+
+        System.out.println("Area = " + area);
+
+    }
+}
