a1ba0021efd58d8cd72876c8fef1cdba6390c528
diaye <yerim-oumar.dia@ssfs.org>
2019-09-16 10:58:25 -0400

    Finished converting pounds to kilograms

---
+++ src/com/company/PoundsToKilograms.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class PoundsToKilograms {
+    public static void main(String[] args) {
+        int lbs = 115;
+        double kilo = lbs/2.205;
+
+        System.out.print(lbs + " pounds" + " = " + kilo + " kilograms" );
+
+    }
+}
